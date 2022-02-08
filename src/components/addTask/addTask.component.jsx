dd6ea28b908e69df891bc11b9972c59cee441ed1
Mohammadreza Asgari <mr.asgari8015@gmail.com>
2022-02-08 14:20:36 +0330

    Added context api

--- src/components/addTask/addTask.component.jsx
+++ src/components/addTask/addTask.component.jsx
@@ -1,18 +1,30 @@
 import React from "react";
+import SimpleContext from "../../Context/SimpleContext";
 
-const AddTask = ({ changeInput, task, addTask }) => {
+const AddTask = () => {
   return (
-      <form onSubmit={event => event.preventDefault()} className="input-group mb-3 justify-content-center">
-        <input
-          type="text"
-          onChange={changeInput}
-          value={task}
-          style={{ height: "100%" }}
-        />
-        <button type="submit" onClick={addTask} style={{ borderRadius: "2px" }}>
-          افزودن به لیست
-        </button>
-      </form>
+    <SimpleContext.Consumer>
+      {({ changeInput , state, addTask }) => (
+        <form
+          onSubmit={(event) => event.preventDefault()}
+          className="input-group mb-3 justify-content-center"
+        >
+          <input
+            type="text"
+            onChange={changeInput}
+            value={state.task}
+            style={{ height: "100%" }}
+          />
+          <button
+            type="submit"
+            onClick={addTask}
+            style={{ borderRadius: "2px" }}
+          >
+            افزودن به لیست
+          </button>
+        </form>
+      )}
+    </SimpleContext.Consumer>
   );
 };
 
